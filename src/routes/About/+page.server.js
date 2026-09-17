7b0d2e2b9b7544435b8c85f47727ee61d27381fa
Angelaaliyah12 <angel.blom@hva.nl>
2026-09-18 01:26:49 +0200

    #4 data ophalen

---
+++ src/routes/About/+page.server.js
@@ -0,0 +1,11 @@
+export async function load() {
+    const response = await fetch(
+        'https://fdnd.directus.app/items/person/238'
+    );
+
+    const data = await response.json();
+    
+    return {
+        person: data.data
+    };
+}
