fd814126ac78df63ba274ae91036f11670ee505c
Angelaaliyah12 <angel.blom@hva.nl>
2026-09-17 12:13:39 +0200

    #3 background toegevoegd

--- myapp/src/routes/+page.svelte
+++ myapp/src/routes/+page.svelte
@@ -1,4 +1,17 @@
+<svelte:body />
 
     <img class="real-img" src="/assets/real1.png" />
 
 <a href="/Game">Game</a>
+
+<style>
+:global(body){
+    /* https://www.w3schools.com/howto/howto_css_full_page.asp */
+ background-image: url("/assets/clouds.gif");
+    height: 100%;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+
+</style>
