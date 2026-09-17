e4f63bd92e9aebfec1a57d7c48eaa0443683c531
Angelaaliyah12 <angel.blom@hva.nl>
2026-09-17 12:10:17 +0200

    #3 game link toegevoegd+ game bestand aangemaakt

--- myapp/src/routes/+page.svelte
+++ myapp/src/routes/+page.svelte
@@ -1,3 +1,4 @@
 
     <img class="real-img" src="/assets/real1.png" />
 
+<a href="/Game">Game</a>
---
+++ myapp/src/routes/Game/+page.svelte
@@ -0,0 +1,56 @@
+<svelte:body />
+
+
+<div class="container">
+    <div class="container-links">
+<a href="/Siblings">Siblings</a>
+<a href="/Afkomst">Afkomst</a>
+</div>
+<img class="game-img" src="/assets/game1.png"/>
+
+    <div class="container-rechts">
+<a href="/Believes">Believes</a>
+<a href="/Weakness">Weakness</a>
+<a href="/">Reset</a>
+    </div>
+</div>
+
+<style>
+:global(body){
+    /* https://www.w3schools.com/howto/howto_css_full_page.asp */
+ background-image: url("/assets/clouds.gif");
+    height: 100%;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+h1{
+    display: flex;
+    justify-self: center;
+}
+.container{
+    display:flex;
+    justify-content: center;
+    align-items: center;
+}
+    .container-links,
+    .container-rechts {
+        display: flex;
+        flex-direction: column;
+        gap:15px;
+    }
+    .game-img{
+        width: 250px;
+        height: auto;
+    }
+    a{
+text-decoration: none;  
+padding: 10px 20px;
+background-color: white;
+border-radius: 10px;
+transition: transform 0.2s ease;
+&:hover{
+    transform: scale(1.3);
+}
+}
+</style>
