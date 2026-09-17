8da2b77b2b146754c55ee4c4a6c475f5381740c5
Angelaaliyah12 <angel.blom@hva.nl>
2026-09-17 13:13:04 +0200

    #5 img toegevoegd + h1

---
+++ myapp/src/routes/Game/+page.svelte
@@ -0,0 +1,29 @@
+<svelte:body />
+
+<h1>Angel</h1>
+
+
+
+<img class="game-img" src="/assets/game1.png"/>
+   
+
+
+<style>
+:global(body){
+ background-image: url("/assets/clouds.gif");
+    height: 100%;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;}
+
+    a{
+        text-decoration: none;
+padding: 10px 20px;
+background-color: white;
+border-radius: 10px;
+transition: transform 0.2s ease;
+&:hover{
+    transform: scale(1.3);
+}
+}
+</style>
