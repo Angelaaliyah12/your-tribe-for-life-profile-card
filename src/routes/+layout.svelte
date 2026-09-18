265fb812d19bde8b7b704ef93066c59e3f62dd36
Angelaaliyah12 <angel.blom@hva.nl>
2026-09-18 10:35:31 +0200

    #6 view transition toegevoegd

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,10 +2,48 @@
 	import favicon from '$lib/assets/favicon.svg';
 
 	let { children } = $props();
+       // https://svelte.dev/blog/view-transitions
+
+	import { onNavigate } from '$app/navigation';
+
+onNavigate((navigation) => {
+	if (!document.startViewTransition) return;
+
+	return new Promise((resolve) => {
+		document.startViewTransition(async () => {
+			resolve();
+			await navigation.complete;
+		});
+	});
+});
 </script>
-
-<svelte:head>
-	<link rel="icon" href={favicon} />
-</svelte:head>
-
 {@render children()}
+
+<style>	
+/* https://css-tricks.com/7-view-transitions-recipes-to-try/ */
+@media (prefers-reduced-motion: no-preference) {
+		:global(::view-transition-old(root)) {
+			animation: circle-wipe-out 1s ease forwards;
+		}
+
+		:global(::view-transition-new(root)) {
+			animation: circle-wipe-in 1s ease forwards;
+		}
+	}
+
+	@keyframes circle-wipe-out {
+		to {
+			clip-path: circle(0% at 50% 50%);
+		}
+	}
+
+	@keyframes circle-wipe-in {
+		from {
+			clip-path: circle(0% at 50% 50%);
+		}
+
+		to {
+			clip-path: circle(150% at 50% 50%);
+		}
+	}
+	</style>
